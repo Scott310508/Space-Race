6be5a1dc6d039a18972f7a77a1b1fcbe89255935
Scott Ngo <anhtngo124@ed.amdsb.ca>
2025-06-05 15:03:29 -0400

    Add project files.

---
+++ Space Race/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Space_Race
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.gameTitle = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.player1ScoreLabel = new System.Windows.Forms.Label();
+            this.player2ScoreLabel = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.winTitle = new System.Windows.Forms.Label();
+            this.timeLeftLabel = new System.Windows.Forms.Label();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.player1Ins = new System.Windows.Forms.Label();
+            this.player2Ins = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.AutoSize = true;
+            this.startButton.BackColor = System.Drawing.Color.Black;
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.startButton.FlatAppearance.BorderSize = 0;
+            this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.startButton.Font = new System.Drawing.Font("Pixelify Sans Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(410, 312);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(304, 107);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = " START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.AutoSize = true;
+            this.quitButton.BackColor = System.Drawing.Color.Black;
+            this.quitButton.Font = new System.Drawing.Font("Pixelify Sans Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quitButton.ForeColor = System.Drawing.Color.White;
+            this.quitButton.Location = new System.Drawing.Point(442, 474);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(247, 107);
+            this.quitButton.TabIndex = 1;
+            this.quitButton.Text = " QUIT";
+            this.quitButton.UseVisualStyleBackColor = false;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // gameTitle
+            // 
+            this.gameTitle.AutoSize = true;
+            this.gameTitle.Font = new System.Drawing.Font("Pixelify Sans", 100.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameTitle.ForeColor = System.Drawing.Color.White;
+            this.gameTitle.Location = new System.Drawing.Point(41, 35);
+            this.gameTitle.Name = "gameTitle";
+            this.gameTitle.Size = new System.Drawing.Size(1001, 202);
+            this.gameTitle.TabIndex = 2;
+            this.gameTitle.Text = "SPACE RACE";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            // 
+            // player1ScoreLabel
+            // 
+            this.player1ScoreLabel.AutoSize = true;
+            this.player1ScoreLabel.Font = new System.Drawing.Font("Pixelify Sans Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.player1ScoreLabel.Location = new System.Drawing.Point(98, 593);
+            this.player1ScoreLabel.Name = "player1ScoreLabel";
+            this.player1ScoreLabel.Size = new System.Drawing.Size(90, 97);
+            this.player1ScoreLabel.TabIndex = 5;
+            this.player1ScoreLabel.Text = "0";
+            this.player1ScoreLabel.Visible = false;
+            // 
+            // player2ScoreLabel
+            // 
+            this.player2ScoreLabel.AutoSize = true;
+            this.player2ScoreLabel.Font = new System.Drawing.Font("Pixelify Sans Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.player2ScoreLabel.Location = new System.Drawing.Point(962, 593);
+            this.player2ScoreLabel.Name = "player2ScoreLabel";
+            this.player2ScoreLabel.Size = new System.Drawing.Size(90, 97);
+            this.player2ScoreLabel.TabIndex = 6;
+            this.player2ScoreLabel.Text = "0";
+            this.player2ScoreLabel.Visible = false;
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Font = new System.Drawing.Font("Pixelify Sans Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.White;
+            this.winLabel.Location = new System.Drawing.Point(516, 312);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(0, 97);
+            this.winLabel.TabIndex = 7;
+            this.winLabel.Visible = false;
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Pixelify Sans Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.ForeColor = System.Drawing.Color.White;
+            this.timerLabel.Location = new System.Drawing.Point(463, 620);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(0, 97);
+            this.timerLabel.TabIndex = 8;
+            this.timerLabel.Visible = false;
+            // 
+            // winTitle
+            // 
+            this.winTitle.AutoSize = true;
+            this.winTitle.Font = new System.Drawing.Font("Pixelify Sans Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winTitle.ForeColor = System.Drawing.Color.White;
+            this.winTitle.Location = new System.Drawing.Point(490, 398);
+            this.winTitle.Name = "winTitle";
+            this.winTitle.Size = new System.Drawing.Size(0, 97);
+            this.winTitle.TabIndex = 9;
+            this.winTitle.Visible = false;
+            // 
+            // timeLeftLabel
+            // 
+            this.timeLeftLabel.AutoSize = true;
+            this.timeLeftLabel.Font = new System.Drawing.Font("Pixelify Sans Medium", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLeftLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLeftLabel.Location = new System.Drawing.Point(442, 604);
+            this.timeLeftLabel.Name = "timeLeftLabel";
+            this.timeLeftLabel.Size = new System.Drawing.Size(138, 97);
+            this.timeLeftLabel.TabIndex = 10;
+            this.timeLeftLabel.Text = "60";
+            this.timeLeftLabel.Visible = false;
+            // 
+            // player2
+            // 
+            this.player2.Image = global::Space_Race.Properties.Resources.spaceships;
+            this.player2.Location = new System.Drawing.Point(796, 620);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(52, 47);
+            this.player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player2.TabIndex = 4;
+            this.player2.TabStop = false;
+            this.player2.Visible = false;
+            // 
+            // player1
+            // 
+            this.player1.Image = global::Space_Race.Properties.Resources.spaceships;
+            this.player1.Location = new System.Drawing.Point(300, 620);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(52, 47);
+            this.player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player1.TabIndex = 3;
+            this.player1.TabStop = false;
+            this.player1.Visible = false;
+            // 
+            // player1Ins
+            // 
+            this.player1Ins.AutoSize = true;
+            this.player1Ins.Font = new System.Drawing.Font("Pixelify Sans Medium", 39F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1Ins.ForeColor = System.Drawing.Color.White;
+            this.player1Ins.Location = new System.Drawing.Point(61, 358);
+            this.player1Ins.Name = "player1Ins";
+            this.player1Ins.Size = new System.Drawing.Size(403, 79);
+            this.player1Ins.TabIndex = 11;
+            this.player1Ins.Text = "P1 Controls";
+            this.player1Ins.Visible = false;
+            // 
+            // player2Ins
+            // 
+            this.player2Ins.AutoSize = true;
+            this.player2Ins.Font = new System.Drawing.Font("Pixelify Sans Medium", 39F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2Ins.ForeColor = System.Drawing.Color.White;
+            this.player2Ins.Location = new System.Drawing.Point(667, 358);
+            this.player2Ins.Name = "player2Ins";
+            this.player2Ins.Size = new System.Drawing.Size(415, 79);
+            this.player2Ins.TabIndex = 12;
+            this.player2Ins.Text = "P2 Controls";
+            this.player2Ins.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1123, 720);
+            this.Controls.Add(this.player2Ins);
+            this.Controls.Add(this.player1Ins);
+            this.Controls.Add(this.timeLeftLabel);
+            this.Controls.Add(this.winTitle);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.player2ScoreLabel);
+            this.Controls.Add(this.player1ScoreLabel);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Controls.Add(this.gameTitle);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Space Race - 1973";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.Label gameTitle;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label player1ScoreLabel;
+        private System.Windows.Forms.Label player2ScoreLabel;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Label winTitle;
+        private System.Windows.Forms.Label timeLeftLabel;
+        private System.Windows.Forms.Label player1Ins;
+        private System.Windows.Forms.Label player2Ins;
+    }
+}
+
---
+++ Space Race/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Space_Race
+{
+    public partial class Form1 : Form
+    {
+        //Components' speeds
+        int playerSpeed = 6;
+        int bulletSpeed = 6;
+
+        //Player scores
+        int player1Score = 0;
+        int player2Score = 0;
+
+        //List of bullets
+        List<Rectangle> bulletList = new List<Rectangle>();
+        List<Rectangle> bulletList2 = new List<Rectangle>();
+        int bulletXSize = 15;
+
+        //Declare moving options
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+        //Declare random generator, sound-player and brush
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        Random randGen = new Random();
+        int randValue = 0;
+        SoundPlayer player = new SoundPlayer();
+        int page = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Set gamescreen resolution
+            this.Size = new Size(900, 600);
+            //Play theme song
+            player = new SoundPlayer(Properties.Resources.theme_song);
+            player.Play();
+            DisplayPage();
+        }
+
+
+        //Start button
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 2;
+            }
+            //timeLeftLabel.Visible = true;
+            gameTitle.Visible = false;
+            player1ScoreLabel.Visible = true;
+            player2ScoreLabel.Visible = true;
+            player1.Location = new Point(this.Width / 4 - player1.Width, 500);
+            player2.Location = new Point(this.Width / 4 * 3 - player2.Width, 500);
+            player1.Visible = true;
+            player2.Visible = true;
+            startButton.Visible = false;
+            quitButton.Visible = false;
+            bulletList.Clear();
+            bulletList2.Clear();
+            Thread.Sleep(3000);
+            DisplayPage();
+            timer1.Start();
+            this.Focus();
+        }
+
+        private void DisplayPage()
+        {
+            switch (page)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    bulletList.Clear();
+                    bulletList2.Clear();
+                    player1Ins.Visible = true;
+                    player2Ins.Visible = true;
+                    Task.Delay(3000);
+                    break;
+            }
+        }
+
+        //Quit button
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+       //Draw bullets
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < bulletList.Count; i++)
+            {
+                e.Graphics.FillRectangle(whiteBrush, bulletList[i]);
+            }
+
+            for (int i = 0; i < bulletList2.Count; i++)
+            {
+                e.Graphics.FillRectangle(whiteBrush, bulletList2[i]);
+            }
+        }
+
+
+        ///Game logic
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //Move player 1
+            if (wDown == true)
+            {
+                player1.Top -= playerSpeed;
+            }
+
+            if (sDown == true)
+            {
+                player1.Top += playerSpeed;
+            }
+
+            //Move player 2
+            if (upArrowDown == true)
+            {
+                player2.Top -= playerSpeed;
+            }
+
+            if (downArrowDown == true)
+            {
+                player2.Top += playerSpeed;
+            }
+
+            //Randomly set y location for bullets from both sides
+            randValue = randGen.Next(1, 100);
+
+            if (randValue < 16)
+            {
+                int y = randGen.Next(100, this.Height - 50);
+                Rectangle bullet = new Rectangle(0, y - 75, 15, 3);
+                bulletList.Add(bullet);
+               
+            }
+
+            if (randValue < 16)
+            {
+                int y = randGen.Next(100, this.Height - 50);
+                Rectangle bullet2 = new Rectangle(this.Width, y - 75, 15, 3);
+                bulletList2.Add(bullet2);
+
+            }
+
+            //Spawn bullets from the sides of the screen
+
+            //From right to left
+            for (int i = 0; i < bulletList.Count; i++)
+            {
+                int x = bulletList[i].X + bulletSpeed;
+                bulletList[i] = new Rectangle(x, bulletList[i].Y, bulletList[i].Width, bulletList[i].Height);
+            }
+            //Remove bullets that go past the screen 
+            for (int i = 0; i < bulletList.Count; i++)
+            {
+                if (bulletList[i].X > this.Width - bulletXSize)
+                {
+                    bulletList.RemoveAt(i);
+                }
+            }
+
+            //Vice versa
+            for (int i = 0; i < bulletList2.Count; i++)
+            {
+                int x = bulletList2[i].X - bulletSpeed;
+                bulletList2[i] = new Rectangle(x, bulletList2[i].Y, bulletList2[i].Width, bulletList2[i].Height);
+            }
+            //Remove bullets that go past the screen 
+            for (int i = 0; i < bulletList2.Count; i++)
+            {
+                if (bulletList2[i].X < 0 + bulletXSize)
+                {
+                    bulletList2.RemoveAt(i);
+                }
+            }
+
+            //Check for collision between players and bullets
+            Rectangle player1Bounds = new Rectangle(player1.Bounds.X, player1.Bounds.Y, player1.Width, player1.Height);
+            Rectangle player2Bounds = new Rectangle(player2.Bounds.X, player2.Bounds.Y, player2.Width, player2.Height);
+            
+            //Reset players' positions after hitting one of the bullets
+            for (int i = 0; i < bulletList.Count; i++)
+            {
+                if (player1Bounds.IntersectsWith(bulletList[i]))
+                {
+                    bulletList.RemoveAt(i);
+                    player1.Location = new Point(this.Width / 4 - player1.Width, 500);
+                }
+            }
+
+            for (int i = 0; i < bulletList2.Count; i++)
+            {
+                if (player1Bounds.IntersectsWith(bulletList2[i]))
+                {
+                    bulletList2.RemoveAt(i);
+                    player1.Location = new Point(this.Width / 4 - player1.Width, 500);
+                }
+            }
+
+            for (int i = 0; i < bulletList.Count; i++)
+            {
+                if (player2Bounds.IntersectsWith(bulletList[i]))
+                {
+                    bulletList.RemoveAt(i);
+                    player2.Location = new Point(this.Width / 4 * 3 - player2.Width, 500);
+                }
+            }
+
+            for (int i = 0; i < bulletList2.Count; i++)
+            {
+                if (player2Bounds.IntersectsWith(bulletList2[i]))
+                {
+                    bulletList2.RemoveAt(i);
+                    player2.Location = new Point(this.Width / 4 * 3 - player2.Width, 500);
+                }
+            }
+
+            //Give players score if they reach the top and reset their postions
+            if (player1.Top < 0 - player1.Width)
+            {
+                player1.Location = new Point(this.Width / 4 - player1.Width, 500);
+                player1Score++;
+                player1ScoreLabel.Text = $"{player1Score}";
+            }
+            if (player2.Top < 0 - player2.Width)
+            {
+                player2.Location = new Point(this.Width / 4 * 3 - player2.Width, 500);
+                player2Score++;
+                player2ScoreLabel.Text = $"{player2Score}";
+            }
+
+            //Check score and stop game if the time is over
+            //for(int timeLeft = 60; timeLeft >= 0;  timeLeft -= 1)
+            //{
+            //    timeLeftLabel.Text += $"{timeLeft}";
+            //}
+            if (player1Score == 3)
+            {
+
+                bulletList.Clear();
+                bulletList2.Clear();
+                player1.Visible = false;
+                player2.Visible = false;
+                gameTimer.Enabled = false;
+                winTitle.Visible = true;
+                player1ScoreLabel.Visible = false;
+                player2ScoreLabel.Visible = false;
+                winTitle.Text = "Player  1 Wins!!";
+                winTitle.Location = new Point(this.Width / 2 - winTitle.Width / 2, this.Height / 2 - winTitle.Height / 2);
+            }
+            else if (player2Score == 3)
+            {
+                bulletList.Clear();
+                bulletList2.Clear();
+                player1.Visible = false;
+                player2.Visible = false;
+                gameTimer.Enabled = false;
+                winTitle.Visible = true;
+                winTitle.Location = new Point(this.Width / 2 - winTitle.Width, this.Height / 2);
+                player1ScoreLabel.Visible = false;
+                player2ScoreLabel.Visible = false;
+                winLabel.Text = "Player  2 Wins!!";
+            }
+            //else
+            //{
+            //    gameTimer.Enabled = false;
+            //    winLabel.Visible = true;
+            //    player1ScoreLabel.Visible = false;
+            //    player2ScoreLabel.Visible = false;
+            //    winLabel.Text = "Tie!!";
+            //}
+            Refresh();
+        }
+
+        
+        //Buttons pressed check
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+        }
+
+
+        //Buttons released check
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+    }
+}
